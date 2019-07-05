626463c5ba1084c0ee781174ca7a53ccb327b146
ashish nagpal <ashishnagpal2498@gmail.com>
2019-07-05 19:13:03 +0530

    Multer file Upload

--- server.js
+++ server.js
@@ -1,7 +1,43 @@
 const express = require('express')
 const ejs = require('ejs')
 const paypal= require('paypal-rest-sdk')
-const config = require('config')
+const config = require('./config')
+const multer = require('multer')
+const path = require('path')
+
+//Set Storing Engine
+const storage = multer.diskStorage({
+    destination: './public_static/uploads',
+    filename :function (req,file,cb) {
+        cb(null,file.fieldname + '-' + Date.now()+path.extname(file.originalname))
+    }
+})
+
+const upload = multer({
+    storage : storage,
+    limits: {fileSize:100000000},
+    fileFilter: function (req,file,cb) {
+        checkFileType(file,cb)
+    }
+}).array('myImage')
+
+function checkFileType(file,cb){
+    //Allowed Extensions
+    const fileTypes = /jpeg|jpg|png/;
+    const extname = fileTypes.test(path.extname(file.originalname).toLowerCase())
+
+//    Check Mime type
+    const mimetype = fileTypes.test(file.mimetype)
+
+    if(mimetype&&extname)
+    {
+        cb(null,true)
+    }
+    else {
+        //This will fill the Error - which is used in Upload Fun
+        cb('Error Images Only|')
+    }
+}
 
 paypal.configure({
     'mode': 'sandbox', //sandbox or live
@@ -11,11 +47,50 @@ paypal.configure({
 
 const app = express();
 
+
+app.use(express.urlencoded({extended:true}))
+app.use(express.json())
+
 app.set('view engine','ejs')
+
+//Public Static Folder
+app.use(express.static('./public_static'))
 
 app.get('/',(req,res)=>{
     res.render('index')
 })
+
+app.get('/multer',(req,res)=> res.render('multer'))
+
+app.post('/upload',(req,res)=>{
+    upload(req,res,(err)=>{
+        console.log(req.body.name)
+        console.log(req.files);
+        if(err)
+        {
+            res.render('multer',{
+                msg: err
+            })
+        }
+        else
+        {
+            if(req.files == undefined)
+            {
+                res.render('multer',{msg:'No file Uploaded'})
+            }
+            else
+            {
+                res.render('multer',{
+                    msg:'File Uploaded',
+                    file : `uploads/${req.files[0].filename}`
+                })
+            }
+            // console.log(req.file)
+            // res.send('TEST')
+        }
+    })
+})
+
 
 app.post('/pay',(req,res)=>{
     const create_payment_json = {
